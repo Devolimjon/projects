cfd3c1593fce43a882a1a62e2c933abcff4e1799
Devolimjon <100147793+Devolimjon@users.noreply.github.com>
2022-03-09 10:45:40 +0500

    Add files via upload

---
+++ online book shop/script.js
@@ -0,0 +1,26 @@
+var Olimjon = "Admin";
+var Muhammad ="Admin 2";
+const email = prompt("Enter Your email");
+const possword = prompt("Enter your password");
+alert(email , possword );
+
+if (email=="olimjonmahmudov26156@gmail.com",
+    possword=="max26156"){
+    alert( "welcome Admin")
+}
+if (email =="",possword==""){
+ alert ("this i account don't found")
+}
+if(email=="muhamadodilbekov2009@gmail.com",
+    possword=="1234567"){
+  alert("Welcome Admin 2")
+}
+if (email == 'sherzodmahmudov@gmail.com',
+    possword =="shermax77"){
+    alert("welcome Mahmudov Sherzod");
+}
+
+if (email=="muniraatamuratov86@gmail.com",
+    possword=="150586Munira"){
+    alert("welcome Atamuratova Munira")
+}
---
+++ online book shop/sign up.js
@@ -0,0 +1,29 @@
+var Olimjon = "Admin";
+var Muhammad ="Admin 2";
+//const email = prompt("your email");
+//const possword = prompt("your password");
+//alert(email , possword );
+
+//if (email=="olimjonmahmudov26156@gmail.com",
+//possword=="max26156"){
+  //  alert( "welcome Admin")
+//}
+//if (email =="",possword==""){
+  //  alert("this account is don't find")
+//}
+//if(email=="muhamadodilbekov2009@gmail.com",
+//possword=="1234567"){
+  //  alert("Welcome Admin 2")
+//}
+//if (email == 'sherzodmahmudov@gmail.com',
+//possword =="shermax77"){
+  //  alert("welcome Mahmudov Sherzod");
+//}
+//
+//if (email=="muniraatamuratov86@gmail.com",
+//possword=="150586Munira"){
+  //  alert("welcome Atamuratova Munira")
+//}
+//else {
+//
+//}
